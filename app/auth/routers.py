d95c9d43c53fc2314d34d3d603bf67dc10a1aee2
Canh Nguyen Tran <canhnt@aiacademy.edu.vn>
2023-03-08 15:47:26 +0700

    Revert this commit

--- app/auth/routers.py
+++ app/auth/routers.py
@@ -1,9 +1,12 @@
+from bson import ObjectId
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.responses import JSONResponse
 from fastapi_jwt_auth import AuthJWT
+from pydash import pick
 
-from app.auth.password import verify_and_update
-from app.user.models import UserLoginModel
-from app.user.services import read_user_by_username, update_user
+from app.auth.password import get_password_hash, verify_and_update
+from app.user.models import UserChangePasswordModel, UserLoginModel
+from app.user.services import get_user_by_id, read_user_by_username, update_user
 
 router = APIRouter()
 
@@ -34,12 +37,46 @@ async def login(body: UserLoginModel, authorize: AuthJWT = Depends()):
     access_token = authorize.create_access_token(subject=str(user["_id"]))
     refresh_token = authorize.create_refresh_token(subject=str(user["_id"]))
 
-    role = user["role"] if "role" in user else None
+    detail = pick(user, "role", "username", "full_name", "avatar_url")
 
     authorize.set_access_cookies(access_token)
     authorize.set_refresh_cookies(refresh_token)
 
-    return HTTPException(status_code=status.HTTP_200_OK, detail={"role": role})
+    return HTTPException(status_code=status.HTTP_200_OK, detail=detail)
+
+
+@router.put("/change-password")
+async def change_user_password(
+    body: UserChangePasswordModel, authorize: AuthJWT = Depends()
+):
+    authorize.jwt_required()
+    user_id = ObjectId(authorize.get_jwt_subject())
+    user = await get_user_by_id(user_id)
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Username or password is valid",
+        )
+
+    verified = verify_and_update(body.password, user["hashed_password"])
+
+    if not verified or verified[0] is False:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Username or password is valid",
+        )
+
+    # TODO: check schemas password
+    hashed_password = get_password_hash(body.new_password)
+    try:
+        await update_user(user_id, {"hashed_password": hashed_password})
+    except:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Username or password is valid",
+        )
+
+    return JSONResponse(status_code=status.HTTP_202_ACCEPTED, content=None)
 
 
 @router.post("/refresh")
