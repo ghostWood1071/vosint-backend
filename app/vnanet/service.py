c81da527e27702358f20637d0019a5e114d6e1ec
Bùi Việt Hoàng <hoangbv@aiacademy.edu.vn>
2023-06-23 08:40:32 +0000

    Fix event

--- app/vnanet/service.py
+++ app/vnanet/service.py
@@ -14,11 +14,36 @@ async def get_all(text_search, start, end, check, skip: int, limit: int):
         start_date = datetime.strptime(start, "%d/%m/%Y")
         end_date = datetime.strptime(end, "%d/%m/%Y")
         query = {"date": {"$gte": start_date, "$lte": end_date}}
-    if check:
-        query = {"is_crawled": check}
+    if check == "All":
+        query = {}
+    if check == "crawled":
+        query = {"is_crawled": True}
+    if check == "not crawl":
+        query = {"is_crawled": False}
     if not query:
         query = {}
     async for item in client.find(query).sort("_id").skip(offset).limit(limit): 
         list_craw.append(item)
     return list_craw
+
+async def count(text_search, start, end, check, skip: int, limit: int):
+    query = {}
+    conditions = []
+    
+    if text_search:
+        conditions.append({"title": {"$regex": text_search, "$options": "i"}})
+    if start and end:
+        start_date = datetime.strptime(start, "%d/%m/%Y")
+        end_date = datetime.strptime(end, "%d/%m/%Y")
+        conditions.append({"date": {"$gte": start_date, "$lte": end_date}})
+    if check == "All":
+        conditions.append({})
+    if check == "crawled":
+        conditions.append({"is_crawled": True})
+    if check == "not crawl":
+        conditions.append({"is_crawled": False})
+    if conditions:
+        query["$and"] = conditions
+    return await client.count_documents(query)
+    
         
