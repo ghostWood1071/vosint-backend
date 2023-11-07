2e771c2ec7992211c0382905c38b48ab8bc69d5e
Thai-Bao Mai-Hoang <maihoangthaibao@aiacademy.edu.vn>
2023-11-07 02:53:47 +0000

    Twitter action

---
+++ vosint_ingestion/automation/actions/twitter.py
@@ -0,0 +1,26 @@
+from ..common import ActionInfo, ActionType, ParamInfo
+from .baseaction import BaseAction
+
+
+class TwitterAction(BaseAction):
+    @classmethod
+    def get_action_info(cls) -> ActionInfo:
+        return ActionInfo(
+            name="twitter",
+            display_name="Twitter",
+            action_type=ActionType.COMMON,
+            readme="twitter",
+            param_infos=[
+                ParamInfo(
+                    name="link_person",
+                    display_name="Link đối tượng theo dõi",
+                    val_type="str",
+                    default_val="",
+                    validators=["required_"],
+                )
+            ],
+            z_index=15,
+        )
+
+    def exec_func(self, input_val=None, **kwargs):
+        pass
