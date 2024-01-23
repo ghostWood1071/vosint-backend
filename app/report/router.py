4c8f49d8a089438ef0b219af9c33f8e3061e3beb
ghostWood1071 <thinhquangshin@gmail.com>
2024-01-23 15:58:59 +0700

    update

--- app/report/router.py
+++ app/report/router.py
@@ -1,55 +1,33 @@
-from typing import Annotated, List, Optional
+from typing import Optional
 
-from bson.objectid import ObjectId
-from fastapi import APIRouter, Body, status
+from fastapi import APIRouter, Body
 from fastapi.params import Depends
-from fastapi.responses import JSONResponse
 from fastapi_jwt_auth import AuthJWT
 import re
 
 from db.init_db import get_collection_client
 
-from .model import CreateQuickReport, CreateReport, GetEvents, UpdateReport
+from .model import CreateReport, UpdateReport, Heading, NewsParams
 from .service import (
-    add_heading_of_report,
+    add_heading_to_report,
     count,
     create_report,
     delete_report,
-    get_event,
     get_report,
     get_reports,
-    remove_heading_of_report,
-    remove_report,
+    remove_heading_from_report,
     update_report,
+    add_news_to_heading,
+    remove_news_from_heading
 )
 
 event_client = get_collection_client("event")
 
 router = APIRouter()
+ 
 
-
-@router.get("/quick-report")
-async def read_reports_quick(
-    title: str = "", skip: int = 0, limit: int = 10, auth: AuthJWT = Depends()
-):
-    auth.jwt_required()
-
-    # get all report with empty user_id
-    query = {
-        "$or": [
-            {"title": {"$regex": title, "$options": "i"}},
-        ],
-        "user_id": {"$exists": False},
-    }
-
-    reports = await get_reports(query, skip, limit)
-    reports_count = await count(query)
-    return {
-        "data": reports,
-        "total": reports_count,
-    }
-
-
+#-
+# get report list
 @router.get("")
 async def read_reports(
     title: str = "", skip: int = 0, limit: int = 10, auth: AuthJWT = Depends()
@@ -72,96 +50,60 @@ async def read_reports(
         "total": reports_count,
     }
 
-
+#get report detail
 @router.get("/{id}")
-async def read_report(id: str):
+async def read_report(id: str, auth: AuthJWT = Depends()):
+    auth.jwt_required()
     report = await get_report(id)
     return report
 
-
-@router.post("/quick-report")
-async def post_quick_report(
-    report: CreateQuickReport = Body(...), auth: AuthJWT = Depends()
-):
-    try:
-        auth.jwt_required()
-        pattern = r'[!@#$%^&*+{}\[\]:;<>,.?~\\|"]'
-        if re.search(pattern, report.title) or report.title.strip() == "":
-            raise Exception("Tiêu đề không chứa ký tự đặc biệt")
-        report_dict = report.dict()
-        print(report_dict)
-        report_created = await create_report(report_dict)
-        return report_created.inserted_id
-    except Exception as e:
-        return JSONResponse(str(e), 500)
-
-
+# add new report 
 @router.post("")
 async def post_report(report: CreateReport = Body(...), auth: AuthJWT = Depends()):
     auth.jwt_required()
     user_id = auth.get_jwt_subject()
-
     report_dict = report.dict()
     report_dict["user_id"] = user_id
     report_created = await create_report(report_dict)
     return report_created.inserted_id
 
-
+#update report
 @router.put("/{id}")
-async def put_report(id: str, data: UpdateReport = Body(...)):
-    event_list = data.event_list
-    del data.event_list
-    eventList = event_list
-    # await event_client.update_many(
-    #     {"list_report": id},
-    #     {"$pull": {"list_report": {"$in": [id]}}},
-    # )
-    for ev in eventList:
-        ev_id = ev
-        await event_client.update_many(
-            {"_id": ObjectId(ev_id)}, {"$addToSet": {"list_report": id}}
-        )
-    # insert id report to event item
+async def put_report(id: str, data: UpdateReport = Body(...), auth: AuthJWT = Depends()):
+    auth.jwt_required()
     report_dict = {k: v for k, v in data.dict().items() if v is not None}
     await update_report(id, report_dict)
     return id
 
-
-@router.put("/add-events-to-heading")
-async def add_head(
-    id_report: Optional[str] = "",
-    id_heading: Optional[str] = "",
-    list_id_event: List[str] = Body(...),
-):
-    list_new = []
-    for item in list_id_event:
-        list_new.append(ObjectId(item))
-    await add_heading_of_report(id_report, id_heading, list_id_event)
-    return 200
-
-
-@router.put("/remove-heading/")
-async def delete_heading(id_report: Optional[str] = "", id_heading: Optional[str] = ""):
-    if id_report and not id_heading:
-        await remove_report(id_report)
-        return JSONResponse(
-            status_code=status.HTTP_200_OK, content="Successful delete a report"
-        )
-    elif id_report and id_heading:
-        await remove_heading_of_report(id_report, id_heading)
-        return 200
-
-
+#delete report
 @router.delete("/{id}")
-async def delete(id: str):
+async def delete(id: str, auth: AuthJWT = Depends()):
+    auth.jwt_required()
     await delete_report(id)
     return id
+#----------------------------------------------------------------------------------------------
+#add an event to heading
+@router.post("/add-heading")
+async def add_heading(heading: Heading, auth: AuthJWT = Depends()):
+    auth.jwt_required()
+    await add_heading_to_report(heading.dict())
+    return 200
+
+# remove a heading
+@router.post("/remove-heading/")
+async def delete_heading(id_report: Optional[str] = "", id_heading: Optional[str] = "", auth: AuthJWT = Depends()):
+    auth.jwt_required()
+    await remove_heading_from_report(id_report, id_heading)
+    return 200
+
+#-----------------------------------------------------------------------------------------------
+@router.post("/add-news-to-heading")
+async def add_news_to_heading_route(data: NewsParams, auth: AuthJWT = Depends()):
+    auth.jwt_required()
+    await add_news_to_heading(**data.dict())
 
 
-@router.post("/get-event-with-new/")
-async def get_event_route(id_count: List[GetEvents]):
-    list_ev = []
-    for item in id_count:
-        list_ev.append(item)
-    reports = await get_event(list_ev)
-    return reports
+@router.post("/remove-news-from-heading")
+async def remove_news_from_heading_route(data: NewsParams, auth: AuthJWT = Depends()):
+    auth.jwt_required()
+    await remove_news_from_heading(**data.dict())
